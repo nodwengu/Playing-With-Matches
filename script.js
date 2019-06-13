6cd264654ffcfbc5f9443cc423e500ffc586c326
Thando <nodwengut@gmail.com>
2019-06-13 15:21:50 +0200

    added 2 more number to the dom

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+var allNumbers = document.querySelectorAll('.number');
+
+var number1Elem = document.querySelector('.number1');
+var number2Elem = document.querySelector('.number2');
+var number3Elem = document.querySelector('.number3');
+
+var msgBtn = document.querySelector('.msgButton');
+
 function getRandom(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
 function updateNumber() {
-    var number1Elem = document.querySelector('.number1');
-    var number2Elem = document.querySelector('.number2');
-    var number3Elem = document.querySelector('.number3');
 
     number1Elem.innerHTML = getRandom(10);
     number2Elem.innerHTML = getRandom(10);
     number3Elem.innerHTML = getRandom(10);
+
+    if(allNumbers[1] == allNumbers[0]) {
+        alert("Equal");
+    } else if(allNumbers[1] == allNumbers[2]) {
+        alert("Equal");
+    }
 }
 
-updateNumber();
+msgBtn.addEventListener('click', updateNumber);
